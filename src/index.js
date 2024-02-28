19fcf6080a0a75090ea4edf46541982d3ef343b2
Md. Shazid Hossain <hossainshazid567@gmail.com>
2024-02-28 17:00:24 +0600

    2nd commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import express from "express";
+import env from "dotenv";
+import { DB_NAME } from "../src/contants.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+// if i want to use import in javascript insted of "require"
+// i have to add "type":"module" in package.json file
+
+const app = express();
+env.config();
+app.use(cors()); //! app.use() is a middleware // which origin i will allow
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cookieParser());
+app.use(express.static("public"));
+app.use(cookieParser());
+
+// app.use(express.json({ limit: "16kb" }));
+// app.use(express.urlencoded({extended : true}))
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.BATMAN_BEGANS}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("ERROR : ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`listening at port ${process.env.PORT || 8000}`);
+    });
+  } catch (error) {
+    console.error("ERROR : ", error);
+    throw error;
+  }
+})();
---
+++ src/utils/AsyncAwaitHandler.js
@@ -0,0 +1,23 @@
+//! method 1 -------------------------------->
+const AsyncAwaitHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { AsyncAwaitHandler };
+
+//! method 2 --------------------------------->
+//! it is a higher order function which can accept a function as a parameter or can return value
+//! basically it is a function in a function
+// const AsyncAwaitHandler = (func) => async (req, res, next) => {
+//     try {
+//       await func(req, res, next);
+//     } catch (error) {
+//       res.status(error.code).json({
+//         success: false,
+//         message: error.message,
+//       });
+//     }
+//   };
+// export { AsyncAwaitHandler };
